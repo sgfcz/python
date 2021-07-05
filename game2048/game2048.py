188ce21737b657d7538c99d5a3cef4f3f7e64646
xiaojiajun <coderxiao@sohu.com>
2021-07-05 23:11:50 +0800

    change 2048game

--- game2048/game2048.py
+++ game2048/game2048.py
@@ -4,5 +4,17 @@ thinker gui
 '''
 import tkinter
 
-top = tkinter.Tk()
-top.mainloop()
+def up():
+    up;
+
+def down():
+    down;
+
+if __name__ == "__main__":
+    top = tkinter.Tk()
+    #TODO 界面
+    #TODO 操控
+    #TODO 显示数值
+    #TODO 游戏是否结束
+
+    top.mainloop()
